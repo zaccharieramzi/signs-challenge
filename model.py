ad5a47fdf6eec09dc83dc31cea0a1504f8edf38a
zaccharieramzi <zaccharie.ramzi@gmail.com>
2017-10-02 22:13:30 +0200

    added model training to the project

---
+++ model.py
@@ -0,0 +1,207 @@
+import tensorflow as tf
+
+
+VGG_MEAN = [103.939, 116.779, 123.68]
+
+
+def constant_weight_variable(data_dict, name):
+    init_val = data_dict[name][0]
+    return tf.get_variable(
+        name + "_weights",
+        initializer=tf.constant(init_val, name="weights"))
+
+
+def constant_bias_variable(data_dict, name):
+    init_val = data_dict[name][1]
+    return tf.get_variable(
+        name + "_biases",
+        initializer=tf.constant(init_val, name="biases"))
+
+
+def weight_variable(shape, name="weights", var_init=0.1):
+    """Wrap weight variable initialization of tensorflow."""
+    return tf.get_variable(
+        name, shape,
+        initializer=tf.truncated_normal_initializer(stddev=var_init))
+
+
+def bias_variable(shape, name="biases"):
+    """Wrapp bias variable initialization of tensorflow."""
+    return tf.get_variable(
+        name, shape, initializer=tf.constant_initializer(0.1))
+
+
+def conv2d(x, W):
+    """Wrap convolution layer initialization of tensorflow."""
+    return tf.nn.conv2d(
+        x, W, strides=[1, 1, 1, 1], padding="SAME", name="convolution")
+
+
+def max_pool_2x2(x):
+    """Wrap the max-pool layer initialization of tensorflow."""
+    ksize = [1, 2, 2, 1]
+    strides = [1, 2, 2, 1]
+    return tf.nn.max_pool(
+        x, ksize=ksize, strides=strides, padding="SAME", name="max-pooling")
+
+
+def conv_layer(x, n_colors, n_kernels, name, reuse=None):
+        """Create a convolutionnal layer."""
+        with tf.variable_scope(name, reuse=reuse):
+            filt = weight_variable(
+                shape=[3, 3, n_colors, n_kernels], name=name + "_filters")
+
+            conv = conv2d(x, filt)
+
+            conv_biases = bias_variable(
+                shape=[n_kernels], name=name + "_biases")
+            bias = tf.nn.bias_add(conv, conv_biases)
+
+            relu = tf.nn.relu(bias)
+            return relu
+
+
+def constant_conv_layer(x, data_dict, name, reuse=None):
+        """Create a constant convolutionnal layer."""
+        with tf.variable_scope(name, reuse=reuse):
+            filt = constant_weight_variable(data_dict, name)
+
+            conv = conv2d(x, filt)
+
+            conv_biases = constant_bias_variable(data_dict, name)
+            bias = tf.nn.bias_add(conv, conv_biases)
+
+            relu = tf.nn.relu(bias)
+            return relu
+
+
+def fc_layer(x, in_size, fc_dim, name, reuse=None):
+    """Create a fully-connected layer."""
+    with tf.variable_scope(name, reuse=reuse):
+        weights = weight_variable(
+            [in_size, fc_dim], name=name + "_weights")
+        biases = bias_variable([fc_dim], name=name + "_biases")
+
+        x = tf.reshape(x, [-1, in_size])
+        y = tf.nn.bias_add(tf.matmul(x, weights), biases)
+    return y
+
+
+def constant_fc_layer(x, data_dict, in_size, name, reuse=None):
+    """Create a constant fully-connected layer."""
+    with tf.variable_scope(name, reuse=reuse):
+        weights = constant_weight_variable(data_dict, name)
+        biases = constant_bias_variable(data_dict, name)
+
+        x = tf.reshape(x, [-1, in_size])
+        y = tf.nn.bias_add(tf.matmul(x, weights), biases)
+    return y
+
+
+def constant_convolutional_layers(
+    input_var, data_dict, n_conv, series_id=0, reuse=None
+):
+    """Create a constant VGG like series of convolutional layers.
+
+    This function creates a series of convolutional layers followed by a max
+    pooling.
+
+    Arguments:
+        - input_var (tf tensor): the input of the convolutional layer.
+        - data_dict (dict): the dictionnary containing the value for each
+        variable.
+        - n_conv (int): the number of convolutions.
+        - series_id (int): the index of the series of convolutional layer in
+        the network. For scoping purposes. Defaults to 0.
+        - reuse (bool): whether we should reuse already instantiated variable
+        or create new ones. Defaults to None.
+
+    Returns:
+        - tf tensor: the convolutional layer.
+
+    """
+    h = input_var
+    for i in range(n_conv):
+        name = "conv{series_id}_{layer_id}".format(
+            series_id=series_id, layer_id=i + 1)
+        h = constant_conv_layer(
+            h, data_dict, name, reuse=reuse)
+    h = max_pool_2x2(h)
+    return h
+
+
+def convolutional_layers(
+    input_var, n_kernels, n_conv, series_id=0, reuse=None
+):
+    """Create a constant VGG like series of convolutional layers.
+
+    This function creates a series of convolutional layers followed by a max
+    pooling.
+
+    Arguments:
+        - input_var (tf tensor): the input of the convolutional layer.
+        - data_dict (dict): the dictionnary containing the value for each
+        variable.
+        - n_kernels (int): the number of kernels in each convolution.
+        - n_conv (int): the number of convolutions.
+        - series_id (int): the index of the series of convolutional layer in
+        the network. For scoping purposes. Defaults to 0.
+        - reuse (bool): whether we should reuse already instantiated variable
+        or create new ones. Defaults to None.
+
+    Returns:
+        - tf tensor: the convolutional layer.
+
+    """
+    h = input_var
+    for i in range(n_conv):
+        name = "conv{series_id}_{layer_id}".format(
+            series_id=series_id, layer_id=i + 1)
+        n_colors = int(h.shape[-1])
+        h = conv_layer(h, n_colors, n_kernels, name, reuse=reuse)
+    h = max_pool_2x2(h)
+    return h
+
+
+def vgg(
+    x, data_dict, architecture_conv, architecture_fc, reuse=None
+):
+    """Create the vgg network."""
+    h = x - VGG_MEAN
+    for series_id, series in enumerate(architecture_conv):
+        h = constant_convolutional_layers(
+            h, data_dict, series["n_conv"],
+            series_id=series_id + 1, reuse=reuse)
+    for fc_id, fc in enumerate(architecture_fc):
+        in_size = fc["in_size"]
+        name = "fc{fc_id}".format(fc_id=fc_id + series_id + 2)
+        h = constant_fc_layer(h, data_dict, in_size, name, reuse=reuse)
+    last_name = "fc{fc_id}".format(fc_id=fc_id + series_id + 3)
+    fc_dim = fc["fc_dim"]
+    y = fc_layer(h, fc_dim, 2, last_name, reuse=reuse)
+    y = tf.nn.softmax(y, name="prob")
+    return y
+
+
+def zsc(
+    x, architecture_conv, architecture_fc, reuse=None
+):
+    """
+    Create the zsc network.
+
+    ZSC stands for Zac simple CNN.
+    """
+    h = x - VGG_MEAN
+    for series_id, series in enumerate(architecture_conv):
+        h = convolutional_layers(
+            h, series["n_kernels"], series["n_conv"],
+            series_id=series_id + 1, reuse=reuse)
+    for fc_id, fc in enumerate(architecture_fc):
+        in_size = fc["in_size"]
+        fc_dim = fc["fc_dim"]
+        name = "fc{fc_id}".format(fc_id=fc_id + series_id + 2)
+        h = fc_layer(h, in_size, fc_dim, name, reuse=reuse)
+    last_name = "fc{fc_id}".format(fc_id=fc_id + series_id + 3)
+    y = fc_layer(h, fc_dim, 2, last_name, reuse=reuse)
+    y = tf.nn.softmax(y, name="prob")
+    return y
---
+++ model_training.py
@@ -0,0 +1,179 @@
+from os.path import join
+import time
+
+import numpy as np
+import tensorflow as tf
+
+from nn import inputs, summary_writer, metrics, records
+from model import vgg, zsc
+
+
+# Datasets aggregation
+train_csv = "data/train.csv"
+test_csv = "data/test.csv"
+train_tfrecord_files, test_tfrecord_files = records(train_csv, test_csv)
+
+
+# Parameters
+# Architecture
+# VGG-16
+architecture_conv = [
+    {"n_conv": 2, "n_kernels": 64},
+    {"n_conv": 2, "n_kernels": 128},
+    {"n_conv": 3, "n_kernels": 256},
+    {"n_conv": 3, "n_kernels": 512},
+    {"n_conv": 3, "n_kernels": 512}
+]
+architecture_fc = [
+    {"in_size": 25088, "fc_dim": 4096},
+    {"in_size": 4096, "fc_dim": 4096}
+]
+
+
+# Parameters
+# Architecture
+# ZSC
+architecture_conv = [
+    {"n_conv": 1, "n_kernels": 16},
+    {"n_conv": 1, "n_kernels": 32},
+    {"n_conv": 1, "n_kernels": 64},
+    {"n_conv": 1, "n_kernels": 128},
+    {"n_conv": 1, "n_kernels": 128}
+]
+architecture_fc = [
+    {"in_size": 6272, "fc_dim": 512},
+    {"in_size": 512, "fc_dim": 512}
+]
+
+
+# Parameters
+# Training
+batch_size = 128
+n_epochs = 2
+n_threads = 2
+l_rate = 0.01
+# Values
+data_dict_path = "models/vgg16.npy"
+# data_dict = np.load(data_dict_path, encoding="latin1").item()
+# Saving and summary
+save_path = "models"
+model_name = "vgg-fine-tuned"
+log_path = "logs"
+log_name = None
+summary_rate = 35
+saving_rate = 35
+
+
+tf.reset_default_graph()
+
+
+with tf.name_scope("train"):
+    x, y_ = inputs(
+        train_tfrecord_files, batch_size,
+        num_epochs=n_epochs, n_threads=n_threads)
+    y_ = tf.reshape(y_, [-1, 2])
+
+    y = zsc(x, architecture_conv, architecture_fc)
+
+with tf.name_scope("test"):
+    x_test, y_test_ = inputs(
+        test_tfrecord_files, batch_size,
+        num_epochs=None, n_threads=n_threads)
+    y_test_ = tf.reshape(y_test_, [-1, 2])
+
+    y_test = zsc(x_test, architecture_conv, architecture_fc, reuse=True)
+
+
+# When training vgg-16, we fine tune the last layer.
+#train_var_names = ["fc8/fc8_weights:0", "fc8/fc8_biases:0"]
+#train_vars = [var for var in tf.trainable_variables() if var.name in train_var_names]
+
+
+with tf.name_scope("loss"):
+    # Define loss function
+    cross_entropy = tf.reduce_mean(
+          tf.nn.softmax_cross_entropy_with_logits(
+            labels=y_, logits=y))
+    cross_entropy_test = tf.reduce_mean(
+          tf.nn.softmax_cross_entropy_with_logits(
+            labels=y_test_, logits=y_test))
+    # Define Optimizer
+    train_step = tf.train.AdamOptimizer(l_rate).minimize(cross_entropy)
+    with tf.name_scope("loss_summary"):
+        tf.summary.scalar("loss", cross_entropy)
+        tf.summary.scalar("loss_test", cross_entropy_test)
+
+
+# Metrics
+with tf.name_scope("metrics"):
+    # Train
+    ground_truth = tf.argmax(y_)
+    predictions = tf.argmax(y)
+    accuracy, _, _, _ = metrics(ground_truth, predictions)
+    # Test
+    ground_truth_test = tf.argmax(y_test_)
+    predictions_test = tf.argmax(y_test)
+    metrics(ground_truth_test, predictions_test, "_test")
+
+
+# Start session
+sess = tf.InteractiveSession()
+init_op = tf.group(
+    tf.global_variables_initializer(),
+    tf.local_variables_initializer())
+sess.run(init_op)
+coord = tf.train.Coordinator()
+threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+
+# Saving
+saver = tf.train.Saver()
+model_name = "vgg_fine_tuned"
+saving_path = join(save_path, model_name)
+
+
+# Summaries
+merged = tf.summary.merge_all()
+writer = summary_writer(log_name, model_name, log_path, sess)
+
+
+try:
+    step = 0
+    while not coord.should_stop():
+        print(step)
+        step_start_time = time.time()
+        # main part of this loop here: train the CNN and get its accuracy.
+        sess.run([train_step])
+        step_duration = time.time() - step_start_time
+        # Print an overview fairly often.
+        if step % summary_rate == 0:
+            kwargs = dict()
+            kwargs["options"] = tf.RunOptions(
+                trace_level=tf.RunOptions.FULL_TRACE)
+            run_metadata = tf.RunMetadata()
+            kwargs["run_metadata"] = run_metadata
+            accuracy_value, summary = sess.run(
+                [accuracy, merged], **kwargs)
+            writer.add_summary(summary, step)
+            writer.add_run_metadata(run_metadata, "step {}".format(step))
+            print("Step {step}: accuracy = {acc} ({duration} sec)".format(
+                step=step,
+                acc=accuracy_value,
+                duration=step_duration)
+            )
+        if step % saving_rate == 0:
+            saver.save(sess, saving_path, global_step=step)
+        step += 1
+except tf.errors.OutOfRangeError:
+    print("Done training for {n_epochs} epochs, {n_steps} steps.".format(
+        n_epochs=n_epochs,
+        n_steps=step
+    ))
+finally:
+    # When done, ask the threads to stop.
+    coord.request_stop()
+
+
+# Wait for threads to finish.
+coord.join(threads)
+sess.close()
