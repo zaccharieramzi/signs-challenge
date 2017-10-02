83ff6b366ee30dbed84ed48bc2813305be6271d8
zaccharieramzi <zaccharie.ramzi@gmail.com>
2017-10-02 17:18:24 +0200

    modified preprocessing function to take into account all the cases

--- preprocessing.py
+++ preprocessing.py
@@ -14,20 +14,30 @@ def pad_image(image, shape=(224, 224, 3)):
     padded_image = np.zeros(shape)
     first_dim = image.shape[0]
     first_dim = first_dim - (first_dim % 2)
+    if first_dim > shape[0]:
+        fit_image = image[
+            (first_dim - shape[0])//2:first_dim-(first_dim - shape[0])//2, :, :
+        ]
+        first_dim = shape[0]
+    else:
+        fit_image = image[:first_dim, :, :]
     second_dim = image.shape[1]
     second_dim = second_dim - (second_dim % 2)
-    if first_dim < shape[0]:
+    if second_dim > shape[1]:
+        fit_2_image = fit_image[
+            :,
+            (second_dim - shape[1])//2:second_dim-(second_dim - shape[1])//2,
+            :
+        ]
+        second_dim = shape[1]
+    else:
+        fit_2_image = fit_image[:, :second_dim, :]
+    if first_dim <= shape[0] or second_dim <= shape[1]:
         padded_image[
             (shape[0] - first_dim)//2:shape[0]-(shape[0] - first_dim)//2,
             (shape[1] - second_dim)//2:shape[1]-(shape[1] - second_dim)//2,
             :
-        ] = image[:first_dim, :second_dim, :]
+        ] = fit_2_image
     else:
-        # for now we crop the image
-        # TODO find way to resize image properly
-        padded_image = image[
-            (first_dim - shape[0])//2:first_dim-(first_dim - shape[0])//2,
-            (second_dim - shape[1])//2:second_dim-(second_dim - shape[1])//2,
-            :
-        ]
+        padded_image = fit_2_image
     return padded_image
